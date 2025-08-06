6a79bc36a30b74428d48eca9d06c26f2641a51d2
ericminnick <121254527+ericminnick@users.noreply.github.com>
2025-08-06 12:16:19 -0500

    Added collision

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,10 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, circle):
+        if (self.position.distance_to(circle.position)) < (self.radius + circle.radius):
+            return True
+        return False
+
+
--- main.py
+++ main.py
@@ -1,27 +1,46 @@
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
 from constants import *
 from player import Player
+import sys
 
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         updatable.update(dt)
+
+        for obj in asteroids:
+            if_true = obj.collision(player)
+            if if_true == True:
+                print("Game over!")
+                sys.exit(0)
+
         screen.fill("black")
+        
         for obj in drawable:
             obj.draw(screen)
+
         pygame.display.flip()
 
         # limit framerate to 60 fps
