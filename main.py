31759830ec81ca0e4b518a828f35617aadba8f7f
ericminnick <121254527+ericminnick@users.noreply.github.com>
2025-08-06 10:56:49 -0500

    Draw player implemented

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,8 +15,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit framerate to 60 fps
+        dt = game_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
